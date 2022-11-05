155badfdf681e0308a2763d619a2119bb12661d5
Oleh Kior <itstudyoleh@gmail.com>
2022-11-05 19:30:31 +0200

    added two objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,12 @@ public class Main {
         Lecture secondLecture = new Lecture();
         Lecture thirdLecture = new Lecture();
         System.out.println(Lecture.LectureCounter);
-        Lecture oneMoreLecture = new Lecture();
-
         Lecture fourthLecture = new Lecture();
         Lecture fifthLecture = new Lecture();
         System.out.println(Lecture.LectureCounter);
+
+
+
 
 
 
