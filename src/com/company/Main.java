364fe747ed262297231c21ed899fef7cc5cfe8b7
Oleh Kior <itstudyoleh@gmail.com>
2022-11-05 18:02:09 +0200

    added one new obcject

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Lecture secondLecture = new Lecture();
         Lecture thirdLecture = new Lecture();
         System.out.println(Lecture.LectureCounter);
+        Lecture oneMoreLecture = new Lecture();
+
 
 
 
